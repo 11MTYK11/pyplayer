bdec1172fa73f87b66a2afe43eb25e132ec76949
Mattuu <96681413+mattuu0@users.noreply.github.com>
2022-09-14 16:56:33 +0900

    Update pyplayer.py

--- pyplayer.py
+++ pyplayer.py
@@ -290,14 +290,10 @@ else:
     lisencelbl = tk.Label(douiwin,text="""                  とりあえず作ったmp3プレイヤー
     このmp3プレイヤーは何となくで作ったソフトですのでところどころ作りこみが甘いです
     また、予期せぬバグとうがあると思います、バージョンアップ等は気が向いたら作ります
-
                 Copyright (c) <2021> <マッツウー>
-
     以下に定める条件に従い、本ソフトウェアおよび関連文書のファイル（以下「ソフトウェア」）の複製を取得するすべての人に対し、ソフトウェアを無制限に扱うことを無償で許可します。
-
     これには、ソフトウェアの複製を使用、複写、変更、結合、掲載、頒布、サブライセンス、および/または販売する権利、およびソフトウェアを提供する相手に同じことを許可する権利も無制限に含まれます。
     上記の著作権表示および本許諾表示を、ソフトウェアのすべての複製または重要な部分に記載するものとします。
-
     ソフトウェアは「現状のまま」で、明示であるか暗黙であるかを問わず、何らの保証もなく提供されます。
     ここでいう保証とは、商品性、特定の目的への適合性、および権利非侵害についての保証も含みますが、それに限定されるものではありません。 
     作者または著作権者は、契約行為、不法行為、またはそれ以外であろうと、ソフトウェアに起因または関連し、
@@ -855,7 +851,7 @@ def logwindow():
 def timeset():
     time = 18
     video.seek(time)
-    audio.seek(time+((time/101)*9))
+    audio.seek(time+((time/101)))
 def fmove(event):
     fxposi.set(event.x)
     fyposi.set(event.y)
@@ -887,7 +883,7 @@ def loopstart(fa,startposin):
         if not startposi == 0:
             timeposi = startposi
             video.seek(timeposi)
-            audio.seek(timeposi+((timeposi/100)*9))
+            audio.seek(timeposi+((timeposi/100)))
         else:
             try:
                 video.stop()
